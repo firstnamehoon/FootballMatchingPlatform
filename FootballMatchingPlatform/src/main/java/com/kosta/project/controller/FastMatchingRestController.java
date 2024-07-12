012636819fcf2a25b6584ebb55755dec7733091b
juho0523 <eqpobibdubdyj@gmail.com>
2024-07-12 15:16:42 +0900

    feat : 리베이스

--- FootballMatchingPlatform/src/main/java/com/kosta/project/controller/FastMatchingRestController.java
+++ FootballMatchingPlatform/src/main/java/com/kosta/project/controller/FastMatchingRestController.java
@@ -1,17 +1,12 @@
 package com.kosta.project.controller;
 
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import com.kosta.project.dto.FastMatchingDTO;
 import com.kosta.project.service.FastMatchingService;
 
-import org.springframework.ui.Model;
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -20,25 +15,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequiredArgsConstructor
 public class FastMatchingRestController {
 
-	private final FastMatchingService fms;
+	private FastMatchingService fms;
 	
-	
-	// 정지 여부 체크하기
-	@PostMapping("/isSuspendedUser")
+	@PostMapping("/issuspendeduser")
 	public Map<String, Boolean> isSuspendedUser(@RequestBody String userId) {
 		//TODO: process POST request
 		
 		return Map.of("result", fms.isSuspendedUser(userId));
 	}
-	
-	
-	// 빠른 신청 리스트
-	@GetMapping("/getFastMatching1")
-	public Map<String, List<FastMatchingDTO>> getFastMatching1() {
-		
-		return Map.of("result", fms.getFastMatchingList());
-	}
-	
 	
 	
 	
--- FootballMatchingPlatform/src/main/java/com/kosta/project/dto/FastMatchingDTO.java
+++ FootballMatchingPlatform/src/main/java/com/kosta/project/dto/FastMatchingDTO.java
@@ -18,5 +18,4 @@ public class FastMatchingDTO {
 	String fieldAddressDetail;
 	String fieldImg;
 	int playerCount;
-	int fieldSeq;
 }
